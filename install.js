ed8165de74750240bdee4d36c3cb470e593cfc89
Rafal Galka <rafal.galka@modernweb.pl>
2018-03-23 12:35:35 +0100

    version change

--- install.js
+++ install.js
@@ -1,10 +1,11 @@
 const spawn = require('child_process').spawnSync;
-
 const version = require('./package.json').version;
-const url = 'http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/'
-  + `${version}/wiremock-standalone-${version}.jar`;
 
-console.log('Downloading WireMock standalone from Maven Central...');
+const wiremockVersion = version.split('-')[0];
+const url = 'http://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/'
+  + `${wiremockVersion}/wiremock-standalone-${wiremockVersion}.jar`;
+
+console.log(`Downloading WireMock standalone from Maven Central...\n  ${url}`);
 
 const wiremockBuild = spawn('curl', [ '-s', '-S', '-L', '-o', './wiremock-standalone.jar', url ], {
   stdio: 'inherit',
